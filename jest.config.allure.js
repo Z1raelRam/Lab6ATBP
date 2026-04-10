fc86e17cb9e08765de23d3c904a2ccf7cb3ba53b
Dima Verokha <dima.veroha@gmail.com>
2026-04-10 11:06:12 +0300

    Force allure-jest v2 and update config

--- jest.config.allure.js
+++ jest.config.allure.js
@@ -1,7 +1,7 @@
 module.exports = {
     testEnvironment: 'node',
     testMatch: ['**/__tests__/**/*.test.js'],
-    // Мы убрали setupFilesAfterEnv, так как allure-jest v3 настраивается через репортеры
+    setupFilesAfterEnv: ['allure-jest/setup'], // Для 2-й версии нужен этот путь
     reporters: [
         'default',
         ['allure-jest', { resultsDir: './allure-results' }]
