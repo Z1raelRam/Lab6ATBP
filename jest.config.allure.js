72709eeee0278a761d021363735c6cdf2dc054db
Dima Verokha <dima.veroha@gmail.com>
2026-04-11 14:06:41 +0300

    LAB_6_ATBP

--- jest.config.allure.js
+++ jest.config.allure.js
@@ -1,7 +1,6 @@
 module.exports = {
     testEnvironment: 'node',
     testMatch: ['**/__tests__/**/*.test.js'],
-    setupFilesAfterEnv: ['allure-jest/setup'], // Для 2-й версии нужен этот путь
     reporters: [
         'default',
         ['allure-jest', { resultsDir: './allure-results' }]
